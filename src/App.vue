3681d38aa4e177d7a4352b933792c3191f61cc29
Ckai <ckai.trans@gmail.com>
2022-12-26 23:03:58 +0800

    feat: add navbar component,  delete default files

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,5 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <h4 class="navbar-brand" href="#">Ti 專業人資系統</h4>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
+  <page-navbar />
   <!-- Header -->
   <header>
     <h4>XXX，你好。今天也是個美好的一天。</h4>
@@ -71,5 +31,16 @@ import HelloWorld from "./components/HelloWorld.vue";
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
 </template>
+
+<script>
+import { RouterLink, RouterView } from "vue-router";
+import PageNavbar from "./components/PageNavbar.vue";
+
+export default {
+  components: {
+    PageNavbar,
+  },
+};
+</script>
 
 <style></style>
