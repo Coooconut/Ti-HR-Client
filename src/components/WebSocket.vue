88a69949c234ae1346b7867adf6f2ab642f9b566
Ckai <ckai.trans@gmail.com>
2023-01-18 09:33:25 +0800

    refactor: adjust before push to remote

--- src/components/WebSocket.vue
+++ src/components/WebSocket.vue
@@ -1,9 +1,4 @@
-<template>
-  <h1>{{ unreadCount }}</h1>
-  <button class="btn btn-primary" @click.prevent="handleSendMessage">
-    發送預設訊息
-  </button>
-</template>
+<template></template>
 
 <script setup>
 import { inject, ref } from "vue";
@@ -11,13 +6,14 @@ import { inject, ref } from "vue";
 const socket = inject("socket");
 let unreadCount = ref(0);
 
-socket.on("connect", () => {
-  console.log(`#connect: ${socket.id}`);
-  unreadCount.value++;
-});
+// WebSocket 的功能目前尚未設計完畢，因此將下列代碼暫時設為註解。
+// socket.on("connect", () => {
+//   console.log(`#connect: ${socket.id}`);
+//   unreadCount.value++;
+// });
 
-function handleSendMessage() {
-  socket.emit("message", "{ text:  '預設訊息'}");
-  console.log("預設訊息已送出");
-}
+// function handleSendMessage() {
+//   socket.emit("message", "{ text:  '預設訊息'}");
+//   console.log("預設訊息已送出");
+// }
 </script>
